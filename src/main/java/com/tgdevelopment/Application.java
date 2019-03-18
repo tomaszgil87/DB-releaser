0e51c405bc0fbab8ee752c0b2202cc46f9788291
tomaszgil87 <tomaszgil87@gmail.com>
2019-03-18 18:15:33 +0100

    Initial

--- src/main/java/com/tgdevelopment/Application.java
+++ src/main/java/com/tgdevelopment/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.sql.DataSource;
 
 import static java.lang.System.exit;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.tgdevelopment")
 public class Application implements CommandLineRunner {
 
     @Autowired
