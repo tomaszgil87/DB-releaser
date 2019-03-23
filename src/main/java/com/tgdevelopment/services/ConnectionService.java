dad8a6dc352173e962acf87c8add2b8ffcec4a93
tomaszgil87 <tomaszgil87@gmail.com>
2019-03-23 22:09:42 +0100

    Update

--- src/main/java/com/tgdevelopment/services/ConnectionService.java
+++ src/main/java/com/tgdevelopment/services/ConnectionService.java
@@ -36,6 +36,7 @@ public class ConnectionService {
 
     public void closeConnectin(Connection connection) throws SQLException {
         connection.close();
+        System.out.println("Connection closed");
     }
 
 }
